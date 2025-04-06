889632cbd7cad0d0beea3e3b9fd12ebd469529e6
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-07 00:37:21 +0200

    Added Under construction notice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 
 function App() {
-  return (
-      <>
-      </>
-  )
+    return (
+        <div className="container mt-5">
+            <div className="alert alert-dark text-center">
+                This site is under construction. Please check back later.
+            </div>
+        </div>
+    )
 }
 
 export default App
