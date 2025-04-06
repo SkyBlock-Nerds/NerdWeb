9e89b2090d1aee16bc0564e762c354aa09628266
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-07 00:59:29 +0200

    Added base axios

---
+++ src/api-client/AxiosInstance.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { getCookie } from './CookieUtils.ts';
+
+// Create an axios instance
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Adds Authorization header to all requests
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = getCookie('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default apiClient;
