ac9afc281781b00370c32f1abd96bce5cc68357d
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-07 18:56:07 +0200

    Added additional potential features

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -11,9 +11,16 @@ function Welcome() {
             <h1>Why isn't this website functional?</h1>
             <p>Currently waiting on <a href="http://github.com/skyblocknerds/nerdbot">NerdBot</a> to get an API.</p>
             <h2>What should be included in the API?</h2>
+            <h3>Required:</h3>
             <p>
                 A get api/generator endpoint for each type of generator with parameters for each generator option (item_name, item_lore, etc.).<br/>
                 A get api/generator/info endpoint which lists all the params and their auto completes.<br/>
+            </p>
+            <h3>Additional:</h3>
+            <p>
+                Endpoints for checking member activity (would require a key/token).<br/>
+                Suggestion search endpoint (would require a key/token).<br/>
+                And possibly more management tools.
             </p>
         </div>
     )
