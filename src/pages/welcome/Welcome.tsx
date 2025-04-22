448efb3aaa1eef9497d48739d5791b9c5cfcd20c
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-22 21:36:33 +0200

    Updated Welcome Screen

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,29 +1,42 @@
 import usePageTitle from "../../hooks/usePageTitle.ts";
+import {useNavigate} from "react-router-dom";
 
 function Welcome() {
-    usePageTitle("");
+    usePageTitle("Welcome - Nerd Bot");
+    const navigate = useNavigate();
 
     return (
         <div className="text-center mt-3 justify-content-center">
-            <h1> Welcome to the Nerd Bot website!</h1>
-            <p> This website will serve as an easier way to use Nerd Bot than using the discord commands.</p>
-            <br/>
-            <h1>Why isn't this website functional?</h1>
-            <p>Currently waiting on <a href="http://github.com/skyblocknerds/nerdbot">NerdBot</a> to get an API.</p>
-            <h2>What should be included in the API?</h2>
-            <h3>Required:</h3>
+            <h1>Welcome to the Nerd Bot website!</h1>
             <p>
-                A get api/generator endpoint for each type of generator with parameters for each generator option (item_name, item_lore, etc.).<br/>
-                A get api/generator/info endpoint which lists all the params and their auto completes.<br/>
+                This website serves as an easier way to use Nerd Bot than using the Discord commands.
+                <br/>
+                <i><sup>UI's on top!</sup></i>
             </p>
-            <h3>Additional:</h3>
+
+            <h2>This website is still in development.</h2>
+            <h5>Want to help out?</h5>
             <p>
-                Endpoints for checking member activity (would require a key/token).<br/>
-                Suggestion search endpoint (would require a key/token).<br/>
-                And possibly more management tools.
+                <a href="http://github.com/skyblock-nerds/nerdweb" target="_blank" rel="noopener noreferrer">
+                    NerdWeb Repo
+                </a> - all contributions are welcome!
             </p>
+            <h5>Bug Reports:</h5>
+            <p>
+                Go to the Nerds Discord and make a bug report in the Bug Report channel.
+                <br/>
+                Not a True nerd?{" "}
+                <a href="https://github.com/SkyBlock-Nerds/NerdWeb/issues/new" target="_blank"
+                   rel="noopener noreferrer">
+                    Create a GitHub Issue
+                </a>
+            </p>
+
+            <button className="btn btn-primary mt-3" onClick={() => navigate("/Image-Generator")}>
+                Get Started
+            </button>
         </div>
-    )
+    );
 }
 
 export default Welcome;
