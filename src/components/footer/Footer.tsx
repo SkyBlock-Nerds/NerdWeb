bcb8fb1eb070b3766d35fac385e7479001747d0d
SocksM <67207504+SocksM@users.noreply.github.com>
2025-05-02 03:05:08 +0200

    added the orange juice repo to the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,6 +6,10 @@ function Footer() {
                     NerdBot GitHub Repo
                 </a>
                 <span>|</span>
+                <a href="https://github.com/Skyblock-Nerds/OrangeJuice/tree/dev" className="text-white mx-2" target="_blank"> //TODO: remove dev branch ref
+                    OrangeJuice GitHub Repo
+                </a>
+                <span>|</span>
                 <a href="https://github.com/Skyblock-Nerds/NerdWeb" className="text-white mx-2" target="_blank">
                     NerdWeb GitHub Repo
                 </a>
