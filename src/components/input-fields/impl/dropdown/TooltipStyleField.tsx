5363fe8bfa9023cd5ddb7ea16a4eaacc9aae628f
SocksM <67207504+SocksM@users.noreply.github.com>
2026-07-28 21:23:47 +0200

    Create TooltipStyleField.tsx

---
+++ src/components/input-fields/impl/dropdown/TooltipStyleField.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import {getTooltipStyleAutoComplete} from "../../../../api-client/requests/GetAutocomplete.ts";
+import DropdownField from "../../DropdownField.tsx";
+
+function TooltipStyleField({value, setValue, texturePack}: { value?: string; setValue: (value: string) => void; texturePack?: string;}) {
+    const [options, setOptions] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchOptions = async () => {
+            const result = await getTooltipStyleAutoComplete(texturePack);
+            setOptions(result);
+        };
+        fetchOptions();
+    }, [texturePack]);
+
+    return (
+        <>
+            <DropdownField
+                value={value}
+                setValue={setValue}
+                options={options}
+                formLabel={"TooltipStyle:"}
+                formName={"tooltipStyle"}
+                formInfo={"Select a TooltipStyle"}
+            />
+        </>
+    );
+}
+
+export default TooltipStyleField;
