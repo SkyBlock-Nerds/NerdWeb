217708954323db58f074f26aaf9a1a3693894cb8
SocksM <67207504+SocksM@users.noreply.github.com>
2026-07-29 18:16:19 +0200

    Readded sidebar toggle (still resizeable)

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -21,12 +21,15 @@ function Sidebar() {
     const [gemstones, setGemstones] = useState<Gemstone[]>([]);
     const [flavors, setFlavors] = useState<Flavor[]>([]);
     const sidebarRef = useRef<HTMLDivElement>(null);
+    const [isOpen, setIsOpen] = useState(true);
 
     const handleMouseDown = () => {
         setIsResizing(true);
     };
 
     useEffect(() => {
+        if (!isOpen) return;
+
         const handleMouseMove = (e: MouseEvent) => {
             if (!isResizing) return;
             const newWidth = window.innerWidth - e.clientX;
@@ -48,7 +51,7 @@ function Sidebar() {
             document.removeEventListener("mousemove", handleMouseMove);
             document.removeEventListener("mouseup", handleMouseUp);
         };
-    }, [isResizing]);
+    }, [isOpen, isResizing]);
 
     useEffect(() => {
         const fetchOptions = async () => {
@@ -61,119 +64,128 @@ function Sidebar() {
     }, []);
 
     return (<>
-        <div
-            className="Sidebar primary-color"
-            ref={sidebarRef}
-            style={{ width: `${width}px` }}
-        >
-            <h2>Cheat sheet</h2>
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Object.entries(colorCodesMap).map(([code, color]) => (<div
-                        key={code}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "10%" }}>{code}</div>
-                        <div
-                            style={{
-                                backgroundColor: color,
-                                width: "20px",
-                                height: "20px",
-                                border: "1px solid black",
-                                margin: "0 0 0 10px"
-                            }}
-                        />
-                    </div>))}
-                </div>}
-                title={"Color Codes"}
-            />
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Object.entries(formatCodesMap).map(([code, name]) => (<div
-                        key={code}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "10%" }}>{code}</div>
-                        <div
-                            className="color-code-item"
-                            style={{ marginLeft: "10px" }}
-                        >
-                            <StyleCodeParser textToBeParsed={code + name} />
-                        </div>
-                    </div>))}
-                </div>}
-                title={"Format Codes"}
-            />
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Array.isArray(icons) && icons.map((icon, index) => (<div
-                        key={index}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "75%" }}>{icon.name}</div>
-                        <div
-                            className="color-code-item"
-                            style={{ fontFamily: "monospace" }}
-                        >
-                            {icon.icon}
-                        </div>
-                    </div>))}
-                </div>}
-                title={"Icons"}
-            />
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Array.isArray(gemstones) && gemstones.map((icon, index) => (<div
-                        key={index}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "75%" }}>{icon.name}</div>
-                        <div
-                            className="color-code-item"
-                            style={{ fontFamily: "monospace" }}
-                        >
-                            {`[` + icon.icon + `]`}
-                        </div>
-                    </div>))}
-                </div>}
-                title={"Gemstones"}
-            />
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Array.isArray(stats) && stats.map((stat, index) => (<div
-                        key={index}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "75%" }}>{stat.name}</div>
-                    </div>))}
-                </div>}
-                title={"Stats"}
-            />
-            <Accordion
-                children={<div className="quaternary-color">
-                    {Array.isArray(flavors) && flavors.map((flavor, index) => (<div
-                        key={index}
-                        style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
-                    >
-                        <div style={{ fontFamily: "monospace", width: "75%" }}>{flavor.name}</div>
-                        <div
-                            className="color-code-item"
-                            style={{ fontFamily: "monospace" }}
-                        >
-                            {flavor.display}
-                        </div>
-                    </div>))}
-                </div>}
-                title={"Flavors"}
-            />
+        {isOpen &&
             <div
-                className="ResizeHandle"
-                onMouseDown={handleMouseDown}
-                style={{ 
-                    right: `${width}px`,
-                }}
-            />
-        </div>
+                className="Sidebar primary-color"
+                ref={sidebarRef}
+                style={{ width: `${width}px` }}
+            >
+                <h2>Cheat sheet</h2>
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Object.entries(colorCodesMap).map(([code, color]) => (<div
+                            key={code}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "10%" }}>{code}</div>
+                            <div
+                                style={{
+                                    backgroundColor: color,
+                                    width: "20px",
+                                    height: "20px",
+                                    border: "1px solid black",
+                                    margin: "0 0 0 10px"
+                                }}
+                            />
+                        </div>))}
+                    </div>}
+                    title={"Color Codes"}
+                />
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Object.entries(formatCodesMap).map(([code, name]) => (<div
+                            key={code}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "10%" }}>{code}</div>
+                            <div
+                                className="color-code-item"
+                                style={{ marginLeft: "10px" }}
+                            >
+                                <StyleCodeParser textToBeParsed={code + name} />
+                            </div>
+                        </div>))}
+                    </div>}
+                    title={"Format Codes"}
+                />
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Array.isArray(icons) && icons.map((icon, index) => (<div
+                            key={index}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "75%" }}>{icon.name}</div>
+                            <div
+                                className="color-code-item"
+                                style={{ fontFamily: "monospace" }}
+                            >
+                                {icon.icon}
+                            </div>
+                        </div>))}
+                    </div>}
+                    title={"Icons"}
+                />
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Array.isArray(gemstones) && gemstones.map((icon, index) => (<div
+                            key={index}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "75%" }}>{icon.name}</div>
+                            <div
+                                className="color-code-item"
+                                style={{ fontFamily: "monospace" }}
+                            >
+                                {`[` + icon.icon + `]`}
+                            </div>
+                        </div>))}
+                    </div>}
+                    title={"Gemstones"}
+                />
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Array.isArray(stats) && stats.map((stat, index) => (<div
+                            key={index}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "75%" }}>{stat.name}</div>
+                        </div>))}
+                    </div>}
+                    title={"Stats"}
+                />
+                <Accordion
+                    children={<div className="quaternary-color">
+                        {Array.isArray(flavors) && flavors.map((flavor, index) => (<div
+                            key={index}
+                            style={{ display: "flex", alignItems: "center", margin: "5px", width: "100%" }}
+                        >
+                            <div style={{ fontFamily: "monospace", width: "75%" }}>{flavor.name}</div>
+                            <div
+                                className="color-code-item"
+                                style={{ fontFamily: "monospace" }}
+                            >
+                                {flavor.display}
+                            </div>
+                        </div>))}
+                    </div>}
+                    title={"Flavors"}
+                />
+                <div
+                    className="ResizeHandle"
+                    onMouseDown={handleMouseDown}
+                    style={{
+                        right: `${width}px`,
+                    }}
+                />
+            </div>
+        }
+        <button
+            className={`SidebarToggle primary-color`}
+            style={{right: isOpen ? width : 0}}
+            onClick={() => setIsOpen(!isOpen)}
+        >
+            {isOpen ? "Close" : "Open"}
+        </button>
     </>);
 }
 
