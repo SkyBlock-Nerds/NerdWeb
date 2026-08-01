9bbf2167343eb9f760423cb7c96f7dc926441861
SocksM <67207504+SocksM@users.noreply.github.com>
2026-08-01 15:31:30 +0200

    Proper indentation in sentry.ts

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.05,
-  replaysOnErrorSampleRate: 1.0,
-  environment: import.meta.env.MODE,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.05,
+    replaysOnErrorSampleRate: 1.0,
+    environment: import.meta.env.MODE,
 });
